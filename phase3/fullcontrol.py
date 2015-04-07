54f0087221b1c1a1fe9390c20a53955a69612046
Steven <stevenboada@gmail.com>
2015-04-07 15:02:18 -0500

    made survey starting more robust
    should not fall out of bounds now

--- phase3/fullcontrol.py
+++ phase3/fullcontrol.py
@@ -35,28 +35,28 @@ def mp_handler(ra, dec):
     #catalog = mk_catalog(tiles)
 
     ### Here is the parallization ###
-    #p = multiprocessing.Pool(1)
+    p = multiprocessing.Pool(multiprocessing.cpu_count())
     #result = p.map(mp_worker_wrapper, izip(gen_pointings(ra, dec),
     #    repeat(catalog, 115*27)))
-    #result = p.map(mp_worker_wrapper, gen_pointings(ra, dec))
+    result = p.map(mp_worker_wrapper, gen_pointings(ra, dec))
 
-    #p.close()
-    #p.join()
+    p.close()
+    p.join()
 
-    result = mp_worker_wrapper(gen_pointings(ra, dec).next())
+    #result = mp_worker_wrapper(gen_pointings(ra, dec).next())
     return rfns.stack_arrays(result, usemask=False)
 
 if __name__ == "__main__":
 # survey bounds
     RAmax = 430
     RAmin = 292
-    DECmax = -10
-    DECmin = -20
+    DECmax = 0
+    DECmin = -10
 
     # here we are making 25 surveys
-    for ra, dec in zip(np.random.rand(1)*(RAmax-RAmin)+RAmin,
-            np.random.rand(1)*(DECmax-DECmin)+DECmin):
-    #for ra, dec in zip([346], [-10]):
+    while 1:
+        ra = np.random.rand(1)*(RAmax-RAmin)+RAmin
+        dec = np.random.rand(1)*(DECmax-DECmin)+DECmin
         if ra > 360:
             ra -= 360.
         print ra, dec
@@ -71,34 +71,36 @@ if __name__ == "__main__":
                 except NameError:
                     tile = find_tile(p[0], p[1], data=data)
                     tile = np.append(tile, find_tile(p[2], p[3], data=data))
+            break
         except ValueError:
             print 'OUT OF BOUNDS!'
-            break
-	tile = np.unique(tile)
-        print tile
-        # load the data -- only the tiles we are going to use
-#        global catalog
-#        catalog = load_tiles(tile)
 
-        # trim down the the data list to make the finding faster
-        # only returns the tiles found in the previous step.
-        myglobals.data = np.asarray([d1 for d1 in data if d1['name'] in tile],
-                dtype=data.dtype)
+    tile = np.unique(tile)
 
-        #############################
-        ### LOAD THE CATALOG DATA ###
-        #############################
-        #myglobals.catalog = mk_catalog(tile)
-        ###################
-        ### DO THE WORK ###
-        ###################
-        print 'do work'
-        result = mp_handler(ra, dec)
-        print len(result)
+    print tile
+    # load the data -- only the tiles we are going to use
+    # global catalog
+    # catalog = load_tiles(tile)
 
-        # write it all to a file
-        print 'ID number is', environ['LSB_JOBID']
-        f = hdf.File('out'+str(environ['LSB_JOBID'])+'.hdf5', 'w')
-        f['dset'] = result
-        f.close()
-        print 'done'
+    # trim down the the data list to make the finding faster
+    # only returns the tiles found in the previous step.
+    myglobals.data = np.asarray([d1 for d1 in data if d1['name'] in tile],
+            dtype=data.dtype)
+
+    #############################
+    ### LOAD THE CATALOG DATA ###
+    #############################
+    #myglobals.catalog = mk_catalog(tile)
+    ###################
+    ### DO THE WORK ###
+    ###################
+    print 'do work'
+    result = mp_handler(ra, dec)
+    print len(result)
+
+    # write it all to a file
+    print 'ID number is', environ['LSB_JOBID']
+    f = hdf.File('out'+str(environ['LSB_JOBID'])+'.hdf5', 'w')
+    f['dset'] = result
+    f.close()
+    print 'done'
