6523b0683ba41b43ff7b885b365fad2e546e7e00
Steven <stevenboada@gmail.com>
2015-04-13 15:38:39 -0500

    initial commit
    chops the halo files into more manageable parts.

--- phase3/haloFiles/chopHaloFiles.py
+++ phase3/haloFiles/chopHaloFiles.py
@@ -11,25 +11,6 @@ def split_list(alist, wanted_parts=1):
     return [ alist[i*length // wanted_parts: (i+1)*length // wanted_parts]
         for i in range(wanted_parts) ]
 
-# load the result file
-f = hdf.File('out1204878.hdf5', 'r')
-dset = f[f.keys()[0]]
-
-# load the coordinate information
-RAmax = dset['RA'].max()
-RAmin = dset['RA'].min()
-DECmax = dset['DEC'].max()
-DECmin = dset['DEC'].min()
-
-# Get the unique haloids
-haloids = np.unique(dset['HALOID'])
-
-# find the tiles that overlap with our area
-#haloTiles = findHaloTile(RAmax, DECmax, RAmin, DECmin)
-
-# loads the halo files
-#haloCat = mk_haloCatalog(haloTiles)
-
 d = fix_haloTiles()
 
 print 'loading...',
@@ -40,16 +21,12 @@ print 'sorting...',
 sortedHids = np.argsort(data['HALOID'])
 print 'done'
 
-# split the files up into 10 parts
-x = split_list(sortedHids, 19)
+# split the files up into 19 parts
+x = split_list(sortedHids, 15)
 
 for idx, p in enumerate(x):
     print idx
     f = hdf.File('halo'+str(idx).zfill(2)+'.hdf5', 'w')
     f[str(idx)] = data[p]
     f.close()
-
-
-
-
 
