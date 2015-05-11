5decb4d8f997665d2ac9d0d4654256d79414ae98
Steven <stevenboada@gmail.com>
2015-05-11 16:42:00 -0500

    format tweak

--- analysis/addClusterProps.py
+++ analysis/addClusterProps.py
@@ -18,58 +18,58 @@ with hdf.File('out1204878_halo.hdf5', 'r') as f:
     dset = f[f.keys()[0]]
     data = dset.value
 
-    # now we need to make a mask for the data
-    mask1 = (data['M200']/0.72 >= 1e13) & (data['Z'] < 0.5)
-    mask2 = (data['g'] < 22.) | (data['Oii'] > 3.5)
-    mask = mask1 & mask2
+# now we need to make a mask for the data -- HETDEX DEPTH!!!
+mask1 = (data['M200']/0.72 >= 1e13) & (data['Z'] < 0.5)
+mask2 = (data['g'] < 22.) | (data['Oii'] > 3.5)
+mask = mask1 & mask2
 
-    # we'll use the mask to make all the changes and then consolidate back.
-    dataMasked = data[mask]
-    #dataMasked = data
-    hids = np.unique(dataMasked['HALOID'])
 
-    halos = find_indices(dataMasked['HALOID'], hids)
+# we'll use the mask to make all the changes and then consolidate back.
+dataMasked = data[mask]
+#dataMasked = data
 
-    for i,h in enumerate(halos):
-        if len(h) < 4:
-            dataMasked['CLUSZ'][h] = np.mean(dataMasked['Z'][h])
-            #find the LOSV
-            dataMasked[h] = findLOSV(dataMasked[h])
+hids = np.unique(dataMasked['HALOID'])
 
-            dataMasked['LOSVD'][h] = np.std(dataMasked['LOSV'][h], ddof=1.5)
+halos = find_indices(dataMasked['HALOID'], hids)
 
-        else:
-            try:
-                #find the cluster redshifts
-                dataMasked[h] = findClusterCenterRedshift(dataMasked[h])
-                #find the LOSV
-                dataMasked[h] = findLOSV(dataMasked[h])
+for i,h in enumerate(halos):
 
-                # find the LOSVD
-                dataMasked['LOSVD'][h] =\
-                astStats.biweightScale_test(dataMasked['LOSV'][h],
-                        tuningConstant=9.0)
+    if len(h) < 5:
+        dataMasked['CLUSZ'][h] = -1.
+        dataMasked['LOSV'][h] = -1.
+        dataMasked['LOSVD'][h] = -1.
 
-            except ZeroDivisionError:
-                badData = -np.ones(len(h))
-                dataMasked['CLUSZ'][h] = badData
-                dataMasked['LOSV'][h] = badData
-                dataMasked['LOSVD'][h] = badData
+    elif 5 <= len(h) < 15:
+        dataMasked[h] = findClusterCenterRedshift(dataMasked[h])
+        dataMasked[h] = findLOSV(dataMasked[h])
+        dataMasked['LOSVD'][h] =\
+            astStats.gapperEstimator(dataMasked['LOSV'][h])
 
-        if not dataMasked['CLUSZ'][h][0] == -1.0:
-            # finally the mass
-            dataMasked['MASS'][h] = calc_mass_Saro(dataMasked[h])
-        else:
-            pass
+    elif 15 <= len(h):
+        #find the cluster redshifts
+        dataMasked[h] = findClusterCenterRedshift(dataMasked[h])
+        #find the LOSV
+        dataMasked[h] = findLOSV(dataMasked[h])
 
-        sys.stdout.write(spinner.next())
-        sys.stdout.flush()
-        sys.stdout.write('\b')
+        # find the LOSVD
+        dataMasked['LOSVD'][h] =\
+            astStats.biweightScale_test(dataMasked['LOSV'][h],
+                    tuningConstant=9.0)
+
+    if not dataMasked['CLUSZ'][h][0] == -1.0:
+        # finally the mass
+        dataMasked['MASS'][h] = calc_mass_Saro(dataMasked[h])
+    else:
+        pass
+
+    sys.stdout.write(spinner.next())
+    sys.stdout.flush()
+    sys.stdout.write('\b')
 
 # now we make another new file
 #with hdf.File('out1204878_complete.hdf5', 'w') as f:
 with hdf.File('out1204878_hetdex.hdf5', 'w') as f:
-    #data[mask] = dataMasked
+#    data[mask] = dataMasked
     f['dset_complete'] = dataMasked
     f.flush()
 
