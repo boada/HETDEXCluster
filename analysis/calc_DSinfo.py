9c01221a1659ee957547e0896b38a9214cccea75
Steven <stevenboada@gmail.com>
2015-05-22 17:16:36 -0500

    make splittable
    added stuff to let me split up the work between different jobs.

--- analysis/calc_DSinfo.py
+++ analysis/calc_DSinfo.py
@@ -4,6 +4,8 @@ from addHaloInfo import find_indices
 from dstest import DStest
 from multiprocessing import Pool, cpu_count, current_process
 from itertools import izip, repeat
+from os import environ
+import sys
 
 def mp_worker((h, data)):
     if len(h) < 10:
@@ -17,7 +19,7 @@ def mp_worker((h, data)):
             del cluster
             return s
         except:
-            print 'DS test problem'
+            print 'DS test problem for HALOID -- ', data['HALOID'][h][0]
             del cluster
             return -2.
 
@@ -27,7 +29,7 @@ def mp_worker_wrapper(*args):
 def start_process():
     print 'Starting', current_process().name
 
-def main():
+def main(start=None, end=None):
 
     #with hdf.File('out1204878_hetdex.hdf5', 'r') as f:
     #with hdf.File('out1204878_complete.hdf5', 'r') as f:
@@ -47,20 +49,23 @@ def main():
 
     p = Pool(cpu_count(), maxtasksperchild=2, initializer=start_process)
 
-    result = p.imap(mp_worker_wrapper, izip(halos, repeat(data)),
+    result = p.imap(mp_worker_wrapper, izip(halos[start:end], repeat(data)),
             chunksize=3)
 
     p.close()
     p.join()
 
-    finalResult = np.ones(hids.shape, dtype=[('HALOID', '>i8'), ('DS', '>f4')])
-    finalResult['HALOID'] = hids
+    finalResult = np.ones(hids[start:end].shape, dtype=[('HALOID', '>i8'),
+        ('DS', '>f4')])
+    finalResult['HALOID'] = hids[start:end]
     finalResult['DS'] = [DS for DS in result]
 
-    with hdf.File('out1204878_allGalaxies_DSresult.hdf5', 'w') as f:
+    #with hdf.File('out1204878_allGalaxies_DSresult1.hdf5', 'w') as f:
+    with hdf.File('out'+str(environ['LSB_JOBID'])+'_allGalaxies_DSresult.hdf5',
+            'w') as f:
         f['DSresult'] = finalResult
         f.flush()
 
     return finalResult
 if __name__ == "__main__":
-    main()
+    main(int(sys.argv[1]), int(sys.argv[2]))
