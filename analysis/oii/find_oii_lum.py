d822736622bdacc4918539a05c5f23bf20fb28ba
Steven <stevenboada@gmail.com>
2015-06-02 17:06:30 -0500

    overhaul to actually work

--- analysis/oii/find_oii_lum.py
+++ analysis/oii/find_oii_lum.py
@@ -1,45 +1,48 @@
 import pylab as pyl
 import pyfits as pyf
 from astLib import astCalc, astStats
+from multiprocessing import Pool, cpu_count
+from itertools import izip
 
-def train(xdat, ydat):
+def absMag(mag, dl):
+    return astCalc.absMag(mag, dl)
+
+def mp_wrapper(args):
+    return absMag(*args)
+
+# start the workers
+p = Pool(cpu_count(), maxtasksperchild=10)
+
+with pyf.open('sdss12_oii_flux_v2.fits') as f:
+    sdssData = f[1].data
+
+    # convert to DES magnitudes
+    g = sdssData['g'] - 0.104 * (sdssData['g'] - sdssData['r']) + 0.01
+    r = sdssData['r'] - 0.102 * (sdssData['g'] - sdssData['r']) + 0.02
+
+    dl = pyl.array(p.map(astCalc.dl, sdssData['redshift'], chunksize=200))
+    xdat = pyl.array(p.map(mp_wrapper, izip(r, dl), chunksize=200))
+    ydat = g - r
+
+    p.close()
+    p.join()
+
     bins = [50,50]
-    thresh = 3
+    extent = [[-26,-10],[-1,4]]
+    _, locx, locy = pyl.histogram2d(xdat, ydat, range=extent, bins=bins)
 
-    hh, locx, locy = pyl.histogram2d(xdat, ydat, range=[[-1,4],[12,22]],
-            bins=bins)
-    posx = pyl.digitize(xdat, locx)
-    posy = pyl.digitize(ydat, locy)
-
-    ind = (posx > 0) & (posx <= bins[0]) & (posy > 0) & (posy <= bins[1])
-    # values of histogram with points in the bins.
-    hhsub = hh[posx[ind] - 1, posy[ind] - 1]
-
-    xdat1 = xdat[ind][hhsub < thresh] # low density points
-    ydat1 = ydat[ind][hhsub < thresh]
-    hh[hh < thresh] = pyl.nan # fill the areas with low density by NaNs
-
-    return locx, locy, hh.T
-
-f = pyf.open('sdss12_oii_flux.fits')
-data = f[1].data
-xdat = data['g'] - data['r']
-ydat = data['r']
-
-# make the training data
-locx, locy, _ = train(xdat, ydat)
-
-# make flux data
-dl = [astCalc.dl(z) for z in data['redshift']]
-dl = pyl.array(dl)
-flux = data['oii_3726_flux']*4*pyl.pi*dl**2
+    # need the Oii luminosity
+    lum = sdssData['oii_3726_flux']*4.*pyl.pi*(dl* 3.0857e24)**2. *1e-17
 
 #####
 ### THIS IS THE PART THAT GETS LOOPED OVER WHEN IT COMES TO THAT ###
 #####
 
-x = 0.49785233
-y = 17.706039
+#x = -17.706039
+#y = 0.49785233
+x = -21.017
+y = 1.24
+
 # find the bins of the color/mag point of interest
 xbin = pyl.digitize([x], locx)
 ybin = pyl.digitize([y], locy)
@@ -48,7 +51,7 @@ ybin = pyl.digitize([y], locy)
 i = (locx[xbin-1] < xdat) & (xdat < locx[xbin]) & (locy[ybin-1] < ydat) & (ydat
         < locy[ybin])
 
-px, x = pyl.histogram(pyl.log10(flux[i]), bins=20, normed=True)
+px, x = pyl.histogram(pyl.log10(lum[i]), bins=20, normed=True)
 x = pyl.linspace(x[0], x[-1], len(px))
 
 s = astStats.slice_sampler(px, N=1, x=x)
