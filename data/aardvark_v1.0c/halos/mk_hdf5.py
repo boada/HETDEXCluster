da0c2b74d6780309977bc40b7c36b4d60517b527
Steven <stevenboada@gmail.com>
2015-07-15 17:07:19 -0500

    reorganize for buzzard

--- data/aardvark_v1.0c/halos/mk_hdf5.py
+++ data/aardvark_v1.0c/halos/mk_hdf5.py
@@ -8,5 +8,6 @@ for f in files:
     print f, '.....',
     t =table.Table.read(f, 'r')
     t.write(f.replace('.fit', '.hdf5'),
-            path=f.split('_')[2]+f.split('_')[4].split('.')[1])
+            path=f.split('_')[1]+f.split('.')[1])
+            #path=f.split('_')[2]+f.split('_')[4].split('.')[1])
     print 'done'
