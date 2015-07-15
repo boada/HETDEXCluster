b3449fbee58e9a08390cb40f5eb0a7dc3422a52d
Steven <stevenboada@gmail.com>
2015-07-15 17:08:02 -0500

    update to add outlined pointing

--- testing/plot_pointingsIFUS.py
+++ testing/plot_pointingsIFUS.py
@@ -12,19 +12,20 @@ if ra > 360:
 fig = pyl.figure()
 ax = fig.add_subplot(111)
 
-# draw the ifu boxes
-for p in gen_pointings(ra, dec):
-    for i in gen_ifus(p[0], p[1]):
-        rec1 = rec((i[0], i[1]), i[2]-i[0], i[3]-i[1], color='#a60628')
-        ax.add_patch(rec1)
+for i, p in enumerate(gen_pointings(ra, dec)):
+    print i,p
 
-    print p
-    gi = gen_ifus(p[0], p[1])
-    i1 = gi.next()
-    x,y = shiftRADec(p[0], p[1], 480., 480.)
+    if i == 7:
+        for i in gen_ifus(p[0], p[1]):
+            rec1 = rec((i[0], i[1]), i[2]-i[0], i[3]-i[1], color='#188487')
+            ax.add_patch(rec1)
+    else:
+        gi = gen_ifus(p[0], p[1])
+        i1 = gi.next()
+        x,y = shiftRADec(p[0], p[1], 480., 480.)
 
-    cir1 = RP((x, y), 8, 480*pyl.cos(pyl.pi/8)**-1/3600., zorder=0, fc='#e24a33',
-    orientation=pyl.pi/8.)
+        cir1 = RP((x, y), 8, 480*pyl.cos(pyl.pi/8)**-1/3600., zorder=0,
+                fc='none', linestyle='dashed', orientation=pyl.pi/8.)
     ax.add_patch(cir1)
 
 pyl.draw()
