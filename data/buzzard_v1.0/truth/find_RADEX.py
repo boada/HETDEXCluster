c747bc3b6f14b09d8e3fdd27c48d8f92234ec2e2
Steven <stevenboada@gmail.com>
2015-07-15 17:07:38 -0500

    initial commit

---
+++ data/buzzard_v1.0/truth/find_RADEX.py
@@ -0,0 +1,26 @@
+from glob import glob
+import h5py as hdf
+from numpy import where
+
+files = glob('*.hdf5')
+
+outFile = open('buzzard_truth.txt', 'w')
+
+for f in files:
+    print f
+    with hdf.File(f, 'r') as f:
+        dset = f[f.keys()[0]]
+        ra = dset['RA']
+        dec = dset['DEC']
+
+        if ra.max() > 300. and ra.min() < 100:
+            x = where(ra < 200)
+            y = where(ra > 200)
+            outFile.writelines('%s %s %s %s %s\n' % (f.keys()[0], ra[x].max(),
+                ra[y].min(), dec.max(), dec.min()))
+        else:
+            outFile.writelines('%s %s %s %s %s\n' % (f.keys()[0], ra.max(),
+                ra.min(), dec.max(), dec.min()))
+
+        f.close()
+outFile.close()
