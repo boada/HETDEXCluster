dd9cc9d847930af0af31d189e17de7b9238a81a4
Steven <stevenboada@gmail.com>
2015-09-21 22:15:49 -0500

    works with new data

--- mkTargeted/submit.py
+++ mkTargeted/submit.py
@@ -1,8 +1,52 @@
 import os
 import time
+from data_handler import mkHalo
+import numpy as np
 
-for i in range(100):
+halo = mkHalo()
+
+mask = (halo['m200c']/0.72 >= 1e13) & (halo['upid'] == -1)
+maskedHalo = halo[mask]
+hids, uniqueIdx = np.unique(maskedHalo['id'], return_index=True)
+
+interval = hids.size//10
+
+print interval
+
+jobscript = \
+        '''
+## send stderr and stdout to the same file
+#BSUB -o output.%J
+
+## login shell to avoid copying env from login session
+## also helps the module function work in batch jobs
+#BSUB -L /bin/bash
+
+## 30 minutes of walltime ([HH:]MM)
+#BSUB -W 02:00
+
+## numprocs
+#BSUB -n 20
+
+## 20 cores/node
+#BSUB -R 'span[ptile=20]'
+
+source /home/boada/.bashrc
+'''
+
+for i in range(10):
+    with open('jobscript'+str(i), 'w') as f:
+        f.write('## job name\n')
+        f.write('#BSUB -J targetedRealistic'+str(i)+'\n')
+        f.write(jobscript)
+        if i == 9:
+            f.write('time py targetedRealistic_async.py '+str(i*interval)+ \
+                    ' '+str(i))
+        else:
+            f.write('time py targetedRealistic_async.py '+str(i*interval)+ \
+                    ' '+str(i*interval+interval))
+
     print i
-    os.system('bsub < jobscript')
-    time.sleep(1)
+    os.system('bsub < jobscript'+str(i))
+    time.sleep(2)
 
