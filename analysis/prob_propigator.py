99a1a9a9a9449079f7ff8374b169c7b70badc402
Steven <stevenboada@gmail.com>
2015-11-22 14:41:02 -0600

    update to work with real data

--- analysis/prob_propigator.py
+++ analysis/prob_propigator.py
@@ -1,17 +1,12 @@
 import numpy as np
 import emcee
 from astLib import astCalc as aca
-from astLib import astStats as ast
+import h5py as hdf
+from sklearn.cross_validation import train_test_split
 
 aca.H0 = 72
 aca.OMEGA_M0 = 0.23
 aca.OMEGA_L0 = 0.77
-
-def findLOSVD(LOSV):
-    if LOSV.size >=15:
-        return ast.biweightScale_test(LOSV, tuningConstant=9.0)
-    else:
-        return ast.gapperEstimator(LOSV)
 
 def calcMass(vd, A1D = 1082.9, alpha=0.3361):
     avgz = 0.0
@@ -53,57 +48,30 @@ def plot_training(mass, sigma, bins=41):
 
     return H, xbins, ybins
 
-N_clusters = 5000
+f = hdf.File('result_targetedIdeal.hdf5', 'r')
+dset  = f[f.keys()[0]]
+data = dset.value
+mask = (np.log10(data['LOSVD']) > 3.12 ) & (data['M200c'] < 10**14.5)
+maskedData = data[~mask]
+badData = data[mask]
+train, test = train_test_split(maskedData, test_size=0.2)
 
-# make the truth
-LOSVDS = np.random.uniform(100, 1200, N_clusters)
-NGALS = np.random.randint(5, 250, N_clusters)
-MASS = np.array(map(calcMass, LOSVDS))
 
-# Now we recover the LOSVDs and this becomes our training sample
-sigmas_obs = np.zeros_like(LOSVDS)
-for i, (vd, ngals) in enumerate(zip(LOSVDS, NGALS)):
-    LOSV = mkLOSVS(vd, ngals)
-    # right now they have zero error
-    LOSV_err = np.zeros_like(LOSV)
-    sigmas_obs[i] = findLOSVD(LOSV)
+# make the joint probability
+H, xbins, ybins = plot_training(np.log10(train['M200c']),
+        np.log10(train['LOSVD']))
+H = H.T
 
 # Now we try to recover, and predict
-expected_mass = np.zeros(N_clusters)
-for j, (vd, ngals) in enumerate(zip(LOSVDS[:10], NGALS[:10])):
-    LOSV = mkLOSVS(vd, ngals)
-    # right now they have zero error
-    LOSV_err = np.zeros_like(LOSV)
-
-    # here is the MCMC stuff
-    ndim = 2  # number of parameters in the model
-    nwalkers = 40  # number of MCMC walkers
-    nburn = 50  # "burn-in" period to let chains stabilize
-    nsteps = 300  # number of MCMC steps to take
-
-    # set theta near the maximum likelihood, with
-    m = np.random.normal(np.mean(LOSV), scale=1, size=(nwalkers))
-    s = np.random.normal(np.std(LOSV), scale=1, size=(nwalkers))
-    starting_guesses = np.vstack([s,m]).T
-
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_posterior, args=[LOSV,
-        LOSV_err], threads=4)
-    sampler.run_mcmc(starting_guesses, nsteps)
-
-    samples = sampler.chain[:, nburn:, :].reshape((-1, ndim))
-#    sigma_rec, mean_rec = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
-#                             zip(*np.percentile(samples, [16, 50, 84],
-#                                                axis=0)))
-
+expected_mass = np.zeros(test.size)
+for j, s in enumerate(test['LOSVD_dist']):
     # using the distribution of sigmas we make P(s) and ds
-    Ps, ds = np.histogram(np.log10(samples[:,0]), bins=50, density=True)
+    Ps, ds = np.histogram(np.log10(s), bins=50, density=True)
     # and get new new sigmas from the histogram2d
     centers = (ds[1:] + ds[:-1])/2.0
 
     # now we have to make P(mt|s) -- from the training sample
     # will need to do this a bunch of times for each sigma in centers
-    H, xbins, ybins = plot_training(np.log10(MASS), np.log10(sigmas_obs))
-    H = H.T
     iy  = np.digitize(centers, ybins) #change this line
     # now we make P(mt) using P(mt|s) and P(s)ds from above
     Pmt = np.zeros(xbins.size -1)
@@ -120,5 +88,4 @@ for j, (vd, ngals) in enumerate(zip(LOSVDS[:10], NGALS[:10])):
     norm = np.sum(Pmt * np.diff(xbins))
     M_expect = np.sum(centers * Pmt * np.diff(xbins))/norm
 
-    print np.log10(MASS[j]),M_expect
-
+    expected_mass[j] = M_expect
