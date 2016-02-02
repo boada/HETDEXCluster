c5db98bddcd3a1079636019444f41317604054c3
Steven <stevenboada@gmail.com>
2016-02-02 10:38:07 -0600

    functions now importable

--- analysis/prob_propigator.py
+++ analysis/prob_propigator.py
@@ -1,8 +1,5 @@
 import numpy as np
 from astLib import astCalc as aca
-from astLib.astStats import slice_sampler
-import h5py as hdf
-from sklearn.cross_validation import train_test_split
 
 aca.H0 = 72
 aca.OMEGA_M0 = 0.23
@@ -18,53 +15,59 @@ def plot_training(mass, sigma, bins=41):
     H, xbins, ybins = np.histogram2d(mass, sigma, bins=[gridx, gridy])
 
     return H, xbins, ybins
+def prob1d(train, test):
 
-f = hdf.File('result_targetedIdeal.hdf5', 'r')
-dset  = f[f.keys()[0]]
-data = dset.value
-mask = (np.log10(data['LOSVD']) > 3.12 ) & (data['M200c'] < 10**14.5)
-maskedData = data[~mask]
-badData = data[mask]
-train, test = train_test_split(maskedData, test_size=0.3)
+    # make the joint probability
+    data = np.column_stack((np.log10(train['M200c']),
+        np.log10(train['LOSVD'])))
 
-# make the joint probability
-H, xbins, ybins = plot_training(np.log10(train['M200c']),
-        np.log10(train['LOSVD']), bins=50)
-H = H.T
+    Ngrid = 41, 21
+    grid = [np.linspace(data[:,i].min(), data[:,i].max(), g+1) for i,g in
+            enumerate(Ngrid)]
 
-# Now we try to recover, and predict
-expected_mass = np.zeros((test.size,), dtype=[('MASS', '>f4'),
-    ('MASS_err', '>f4', (2,))])
+    H, (xbins, ybins) = np.histogramdd(data, bins=grid)
 
-for j, s in enumerate(test['LOSVD_dist']):
-    # using the distribution of sigmas we make P(s) and ds
-    Ps, ds = np.histogram(np.log10(s), bins=50, density=True)
-    # and get new new sigmas from the histogram2d
-    centers = (ds[1:] + ds[:-1])/2.0
+    # The bin order in the histogram now becomes z, y, x!!!
+    H = H.T
 
-    # now we have to make P(mt|s) -- from the training sample
-    # will need to do this a bunch of times for each sigma in centers
-    iy  = np.digitize(centers, ybins) #change this line
-    # now we make P(mt) using P(mt|s) and P(s)ds from above
-    Pmt = np.zeros(xbins.size -1)
-    Psds = Ps*np.diff(ds)
-    for i in range(iy.size):
-        try:
-            Pmt_s = H[iy[i]] / H[iy[i]].sum()
-        except IndexError:
-            Pmt_s = np.zeros(xbins.size -1)
-        Pmt += Pmt_s * Psds[i]
+    # Now we try to recover, and predict
+    expected_mass = np.zeros((test.size,), dtype=[('MASS', '>f4'),
+        ('MASS_err', '>f4', (2,))])
 
-    # now we can calculate the expected mass
-    centers = (xbins[:-1] + xbins[1:])/2.
-    norm = np.sum(Pmt * np.diff(xbins))
-    M_expect = np.sum(centers * Pmt * np.diff(xbins))/norm
+    for j, s in enumerate(test['LOSVD_dist']):
+        # using the distribution of sigmas we make P(s) and ds
+        Ps, ds = np.histogram(np.log10(s), bins=50, density=True)
+        # and get new new sigmas from the histogram2d
+        centers = (ds[1:] + ds[:-1])/2.0
 
-    variance = np.sum((centers-M_expect)**2 * Pmt* np.diff(xbins))/norm
+        # now we have to make P(mt|s) -- from the training sample
+        # will need to do this a bunch of times for each sigma in centers
+        iy  = np.digitize(centers, ybins) #change this line
+        # now we make P(mt) using P(mt|s) and P(s)ds from above
+        Pmt = np.zeros(xbins.size -1)
+        Psds = Ps*np.diff(ds)
+        for i in range(iy.size):
+            try:
+                Pmt_s = H[iy[i]] / H[iy[i]].sum()
+            except IndexError:
+                Pmt_s = np.zeros(xbins.size -1)
+            Pmt += Pmt_s * Psds[i]
 
-    expected_mass['MASS'][j] = M_expect
-    expected_mass['MASS_err'][j] = [M_expect - np.sqrt(variance), M_expect
-            + np.sqrt(variance)]
-#    resamples = slice_sampler(Pmt,x=centers, N=1000)
-#    expected_mass['MASS_err'][j] = np.percentile(resamples, [16, 84])
+        # now we can calculate the expected mass
+        centers = (xbins[:-1] + xbins[1:])/2.
+        norm = np.sum(Pmt * np.diff(xbins))
+        M_expect = np.sum(centers * Pmt * np.diff(xbins))/norm
 
+        variance = np.sum((centers-M_expect)**2 * Pmt* np.diff(xbins))/norm
+
+        expected_mass['MASS'][j] = M_expect
+        expected_mass['MASS_err'][j] = [M_expect - np.sqrt(variance), M_expect
+                + np.sqrt(variance)]
+
+        #mask = np.where(np.isnan(expected_mass['MASS']))[0]
+        #expected_mass = np.delete(expected_mass, mask)
+        #test = np.delete(test, mask)
+
+    return expected_mass
+if __name__ == "__main__":
+    prob1d()
