124b90dae320cfd06256f0ca03a04db3d8187143
Steven <stevenboada@gmail.com>
2016-02-25 13:43:53 -0600

    initial commit
    makes the probability method plot

---
+++ analysis/plot_prob_example.py
@@ -0,0 +1,44 @@
+import pylab as pyl
+import h5py as hdf
+import corner
+
+
+### Targeted ###
+################
+with hdf.File('./result_targetedPerfect.hdf5', '#a60628') as f:
+    dset  = f[f.keys()[0]]
+    #data = dset['IDX', 'HALOID', 'ZSPEC', 'M200c', 'NGAL', 'LOSVD',
+    #    'LOSVD_err', 'MASS', 'LOSVD_dist']
+    data = dset['ZSPEC', 'M200c', 'LOSVD',]
+
+mask = ((pyl.log10(data['LOSVD']) > 3.12 ) & (data['M200c'] < 10**14.5) |
+    (data['LOSVD'] < 50))
+data = data[~mask]
+badData = data[mask]
+
+X = pyl.column_stack([data['ZSPEC'], pyl.log10(data['M200c']),
+    pyl.log10(data['LOSVD'])])
+f = corner.corner(X, labels=['z', 'Log $M_{200c}$', 'Log $\sigma$'], bins=50,
+        smooth=True, fill_contours=True)
+
+
+# add the bars. The axes are row indexed
+
+axes = f.axes
+
+# redshift
+axes[0].axvspan(0.15,0.19,facecolor='#a60628', alpha=0.25)
+axes[3].axvspan(0.15,0.19,facecolor='#a60628', alpha=0.25)
+axes[6].axvspan(0.15,0.19,facecolor='#a60628', alpha=0.25)
+
+# LOSVD
+axes[6].axhspan(2.55,2.65,facecolor='#a60628', alpha=0.25)
+axes[7].axhspan(2.55,2.65,facecolor='#a60628', alpha=0.25)
+axes[8].axvspan(2.55,2.65,facecolor='#a60628', alpha=0.25)
+
+# mass
+axes[3].axhspan(13.25,13.5,facecolor='#a60628', alpha=0.25)
+axes[7].axvspan(13.25,13.5,facecolor='#a60628', alpha=0.25)
+axes[4].axvspan(13.25,13.5,facecolor='#a60628', alpha=0.25)
+
+
