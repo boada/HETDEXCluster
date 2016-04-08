a2c42f9b5b0647c38883a046c85afad45062c0f5
Steven <stevenboada@gmail.com>
2016-04-08 13:12:05 -0500

    better calculation of the errors

--- analysis/calc_errors_ML.py
+++ analysis/calc_errors_ML.py
@@ -1,15 +1,26 @@
 import numpy as np
 import h5py as hdf
+from scipy import stats
 
-def scatter(true, pred, mu):
-    if true.size > 0:
+def error(true, pred, mu):
+    ''' Unused, but kept to see how I did it when I wasn't using the Scipy
+    functions. Calculates the error on the mean.
+
+    '''
+    print true.size,
+
+    if true.size > 1:
         var = np.sum((pred - true - mu)**2) /(true.size - 1)
         sem = np.sqrt(var/true.size)
         return sem
+    elif true.size == 1:
+        return 0
     else:
         return np.nan
 
 def bias(true, pred):
+    ''' unused, but calculates the mean bias. '''
+
     if true.size > 0:
        return np.sum(pred - true) /true.size
         #return np.median(true)
@@ -17,21 +28,24 @@ def bias(true, pred):
         return np.nan
 
 def runningStatistic(stat, true, pred, **kwargs):
+    ''' b = bias and s = uncertainty on that bias '''
+
     bins = np.arange(11.5,16,0.5)
-    indx = np.digitize(true, bins) - 1
+    indx = np.digitize(true, bins)-1
     binNumber = len(bins)
 
     runningb = []
     runnings = []
     for k in xrange(binNumber):
+        #print true[indx==k].size,
+        b = np.mean(pred[indx==k] - true[indx==k])
+        s = stats.sem(pred[indx==k] - true[indx==k])
+        #print '$%.2f\pm{%.4f}$ &' % (b,s)
         try:
-            b = stat(true[indx==k], pred[indx==k], **kwargs)
-            s = scatter(true[indx==k], pred[indx==k], b)
+            mean, var, std = stats.mvsdist(pred[indx==k] - true[indx==k])
+            print '$%.2f\pm{%.2f}$ &' % (std.mean(),std.std()),
         except ValueError:
-            b = np.nan
-            s = np.nan
-        print '$%.2f\pm{%.2f}$ &' % (b,s),
-        #print '%.3f' % (s), '&',
+            print '$%.2f\pm{%.2f}$ &' % (np.nan,np.nan),
         runningb.append(b)
         runnings.append(s)
     print ''
@@ -68,9 +82,9 @@ survey = survey[mask]
 for d in [perfect, target, survey]:
 
 ### Full survey ###
-    mu = bias(np.log10(d['M200c']), np.log10(d['MASS']))
-    s = scatter(np.log10(d['M200c']), np.log10(d['MASS']), mu)
-    print '$%.2f\pm{%.3f}$' % (mu,s)
+#    mu = bias(np.log10(d['M200c']), np.log10(d['MASS']))
+#    s = scatter(np.log10(d['M200c']), np.log10(d['MASS']), mu)
+#    print '$%.2f\pm{%.3f}$' % (mu,s)
 
 
     print('power law')
